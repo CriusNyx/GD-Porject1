7a4479fdd1ccb5976e30131a8188314668ec8405
DLidz <46999371+DLidz@users.noreply.github.com>
2020-09-14 21:25:43 -0400

    Create Boundaries.cs

---
+++ Assets/Scripts/Player/Boundaries.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        if(pos.x > 10.5)
+        {
+            pos.x = 10.5F;
+        }
+        if (pos.x < -10.5F)
+        {
+            pos.x = -10.5F;
+        }
+        if (pos.y > 5)
+        {
+            pos.y = 5;
+        }
+        if (pos.y < -5)
+        {
+            pos.y = -5;
+        }
+        transform.position = pos;
+    }
+
+}
